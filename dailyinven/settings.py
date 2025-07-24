8debb20c657cb60a7905bcdfe97a8f4d7a3ea389
Fiti-git <work@fathitech.com.lk>
2025-07-24 13:07:05 +0530

    new db crendtinals

--- dailyinven/settings.py
+++ dailyinven/settings.py
@@ -85,13 +85,23 @@ WSGI_APPLICATION = 'dailyinven.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'DI',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'DI',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        'HOST': 'db',  # <- fix here
         'PORT': '5432',
     }
 }
